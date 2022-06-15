76d10461d809190649b8b65187bece1e3db956e8
yujinkim11 <wowyj26@naver.com>
2022-06-15 09:57:11 +0900

    props

--- src/App.js
+++ src/App.js
@@ -12,12 +12,22 @@ const App = () => {
     <div>
       {/* <TagRule /> */}
       {/* 1일차    */}
-
       {/* <FnComponent /> */}
       {/* <FnComponent_1 />
       <FnComponent_2 /> */}
       {/* <ClassComponent /> */}
-      <FoodMenu />
+
+      {/* * props => <컴포넌트 프롭스명="값" /> */}
+      <h3>아침메뉴</h3>
+      <FoodMenu menuName="계란밥" SideName="아메리카노" />
+
+      <h3>점심메뉴</h3>
+      <FoodMenu menuName="햄버거" SideName="콜라" />
+
+      <h3>저녁메뉴</h3>
+      <FoodMenu menuName="마라탕" SideName="홍차" />
+
+      {/* 2일차 */}
     </div>
   );
 };
--- src/components/2_component/FoodMenu.js
+++ src/components/2_component/FoodMenu.js
@@ -1,3 +1,13 @@
-export const FoodMenu = () => {
-  return <h3>오늘의 메뉴는 텐동</h3>;
+import "../../styles/style.css";
+
+export const FoodMenu = ({ menuName, SideName }) => {
+  // console.log(menuName);
+  // => 프롭스는 오브젝트 형태로 가져오기 때문에 매개변수에 중괄호를 적고 작성할 것
+  return (
+    <div>
+      오늘의 메뉴는 <i className="main">{menuName}</i> 사이드 메뉴는
+      <i className="side">{SideName}</i>
+      {/* => jsx 안쪽에 변수를 사용할 시 중괄호로 감싸줄 것 */}
+    </div>
+  );
 };
