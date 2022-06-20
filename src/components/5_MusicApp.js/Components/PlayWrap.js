2681cf3619fedddce89ed6a2a8819101da05a062
yujinkim11 <wowyj26@naver.com>
2022-06-20 10:38:45 +0900

    PlayWrap

--- src/components/5_MusicApp.js/Components/PlayWrap.js
+++ src/components/5_MusicApp.js/Components/PlayWrap.js
@@ -1,10 +1,29 @@
 import styled from "styled-components";
 
-const SPlayWrap = styled.div``;
+const SPlayWrap = styled.div`
+  width: 100%;
+  margin-top: 30px;
+`;
 
-const GaugeWrap = styled.div``;
+const GaugeWrap = styled.div`
+  width: 100%;
+  height: 5px;
+  background-color: gray;
+`;
 
-const Gauge = styled.div``;
+const Gauge = styled.div`
+  width: 10%;
+  height: 100%;
+  background-color: mediumseagreen;
+`;
+
+const TimeWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5px;
+  font-size: 12px;
+  font-weight: 100;
+`;
 
 export const PlayWrap = () => {
   return (
@@ -12,8 +31,10 @@ export const PlayWrap = () => {
       <GaugeWrap>
         <Gauge></Gauge>
       </GaugeWrap>
-      <span>00:03</span>
-      <span>03:40</span>
+      <TimeWrap>
+        <span>00:03</span>
+        <span>03:40</span>
+      </TimeWrap>
     </SPlayWrap>
   );
 };
